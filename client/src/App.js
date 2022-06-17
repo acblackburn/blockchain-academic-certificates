143ec65baca4d8c573d5d289ddf0ba9764016096
acblackburn <alexcblackburn98@gmail.com>
2022-06-17 19:25:18 +0100

    Create basic React/Truffle project

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Blockchain Academic Credentials</h1>
+    </div>
+  );
+}
+
+export default App;
