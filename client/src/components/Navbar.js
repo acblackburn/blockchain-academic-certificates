8633e950206c5965ad153676d62bb5b5d4c0ad2a
acblackburn <alexcblackburn98@gmail.com>
2022-06-18 15:42:15 +0100

    Add functionality to connect to browser wallet

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,36 @@
+import { useState } from "react";
+import Web3 from 'web3';
 
 function Navbar() {
+
+  const [account, setAccount] = useState(0);
+  const [isConnected, setConnected] = useState(false);
+
+  async function getAccount() {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+    setAccount(account);
+    setConnected(true);
+  }
+
   return (
     <div className="fixed top-0 h-12 w-screen m-0 flex flex-row justify-between items-center bg-gray-800">
       <h1 className="ml-4 text-slate-200 font-bold">
         blockchain-academic-credentials
       </h1>
       <div className="flex flex-row items-center">
-        <button className="mr-4 px-2 py-1 text-slate-200 font-bold bg-blue-500 rounded-full hover:bg-blue-700">
+        <button
+          onClick={getAccount}
+          className="mr-4 px-2 py-1 text-slate-200 font-bold bg-blue-500 rounded-full hover:bg-blue-700"
+          >
           Connect Wallet
         </button>
-        <h1 className="mr-4 text-slate-200 font-bold">
-          0x0
-        </h1>
+        {isConnected &&
+          <h1 className="mr-4 text-slate-200 font-bold">
+            {account}
+          </h1>
+        }
+    
       </div>
     </div>
   );
