daba73ebff515902c1bb89f464acbdded95c14dc
acblackburn <alexcblackburn98@gmail.com>
2022-06-18 11:31:37 +0100

    Install tailwindcss and setup in project

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
 import './App.css';
 
 function App() {
+
+  // useEffect (() => {
+  //   const accounts = window.ethereum.request({ method: 'eth_requestAccounts' });
+  //   const account = accounts[0];
+  // });
+
   return (
     <div className="App">
-      <h1>Blockchain Academic Credentials</h1>
+      <h1 className="text-3xl font-bold underline">
+        Blockchain Academic Credentials
+      </h1>
     </div>
   );
 }
