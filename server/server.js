8745d2aaa05127af2915baf11f4ae677df3f8c34
acblackburn <alexcblackburn98@gmail.com>
2022-07-18 17:47:49 +0100

    Add API calls to fetch and add new hashes to server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,21 @@ const PORT = 3001;
 
 const app = express();
 
+let hashes = ['a', 'b', 'c', 'd', 'e'];
+
+app.get('/hashes', (req, res) => {
+  res.json(hashes);
+});
+
+app.post('/hashes', (req, res) => {
+  const hash = req.body;
+
+  console.log(hash);
+  hashes.push(book);
+
+  res.send("Hash has been added to the database");
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
