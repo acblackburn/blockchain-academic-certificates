42bcf60b110f6bdf16fe128d79b123a442d99ab5
acblackburn <alexcblackburn98@gmail.com>
2022-08-14 17:33:04 +0100

    Modify Publish page to correctly load CIDs from gunDB. Set up dedicated infura IPFS gateway
    Load CIDs from gun.js
    * Get all CIDs from gun.js in realtime and store in an array in state
    * Remove merkleTree from state. Instead generate upon new file publish event
    * Alert user if CID of file to be published already exists in gun
    
    Set up dedicated infura IPFS gateway
    * Infura have removed the public non-authenticated IPFS gateway as of 10/08/2022
    * Instead, set up dedicated IPFS gateway for project which required authentication
    * Infura project API key stored in environment variables

--- client/src/pages/Publish.js
+++ client/src/pages/Publish.js
@@ -1,20 +1,46 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import Gun from 'gun';
 import keccak256 from 'keccak256';
 import Web3 from 'web3';
-import MerkleTree from 'merkletreejs';
+import { MerkleTree } from 'merkletreejs';
 import { create } from 'ipfs-http-client';
 import VerifyCertificate from '../contracts_build/contracts/VerifyCertificate.json';
 
 function Publish(props) {
 
-  const [contract, setContract] = useState(0);
-  const [merkleTree, setMerkleTree] = useState(0);
   const [file, setFile] = useState(null);
+  const [CIDs, setCIDs] = useState([]);
   const [publishStatus, setPublishStatus] = useState(0);
 
+  // create ETH web3 object
   const web3 = new Web3(Web3.givenProvider);
-  const client = create('https://ipfs.infura.io:5001/api/v0');
+
+  const auth =
+    'Basic ' + Buffer.from(process.env.REACT_APP_IPFS_PROJECT_ID + ':' + process.env.REACT_APP_IPFS_API_KEY).toString('base64');
+
+  // create object to access IPFS infura dedicated gateway
+  const client = create({
+    host: 'ipfs.infura.io',
+    port: 5001,
+    protocol: 'https',
+    headers: {
+      authorization: auth,
+    },
+  });
+
+  // Init connection to gunDB with relay server and get certificatesData set
+  const gun = Gun({peers: ['http://localhost:3001/gun']});
+  const certificatesData = gun.get('certificatesData');
+
+  useEffect(() => {
+    // Load all certificate CIDs from gunDB (gets updates in realtime)
+    certificatesData.map().on((node, CID) => {
+      if (node && !CIDs.includes(CID)) {
+        setCIDs(CIDs.concat(CID));  
+      }
+    });
+  })
 
   const retrieveFile = (e) => {
     e.preventDefault();
@@ -29,42 +55,40 @@ function Publish(props) {
   const handleFileUpload = async (e) => {
     e.preventDefault();
     try {
+      // Load VerifyCertificate solidity contract
+      const networkId = await web3.eth.net.getId();
+      const networkData = VerifyCertificate.networks[networkId];
+      const contract = new web3.eth.Contract(VerifyCertificate.abi, networkData.address, { from: window.ethereum.selectedAddress });
+
+      // Add uploaded file to IPFS
       const fileAdded = await client.add(file);
       console.log("Added file:", fileAdded.path);
-      
-      fetch("/CIDs")
-      .then((res) => res.json())
-      .then((CIDs) => {
-        const leaves = CIDs.map(CID => keccak256(CID));
-        leaves.push(keccak256(fileAdded.path));
-        setMerkleTree(new MerkleTree(leaves, keccak256, { sort: true }));
-      });
 
+      if (CIDs.includes(fileAdded.path)) {
+        alert("This certificate has already been published!");
+        return;
+      }
+
+      // Hash all CIDs into leaves array and add the hashed CID of the new certificate
+      const leaves = CIDs.map(CID => keccak256(CID));
+      leaves.push(keccak256(fileAdded.path));
+      console.log(leaves);
+
+      // Create merkle tree using leaves array
+      const merkleTree = new MerkleTree(leaves, keccak256, { sort: true });
+      
+      // Get the new merkle root and call the set method from the smart contract to save to the blockchain
       const newRoot = merkleTree.getHexRoot();
       console.log(newRoot);
-      contract.methods.setRoot(newRoot).send();
+      await contract.methods.setRoot(newRoot).send();
 
-      fetch('/CID', { 
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({CID: fileAdded.path}),
-      })
+      // Add new certificate metadata to gunDB
+      const newCertificate = gun.get(fileAdded.path).put({studentAccount: "exampleStudentAccount", uploaderAccount: "exampleUploaderAccount"});
+      certificatesData.set(newCertificate);
     } catch (error) {
       console.log(error.message);
     }
   }
-
-  useEffect(() => {
-    const loadContract = async () => {
-      const networkId = await web3.eth.net.getId();
-      const networkData = VerifyCertificate.networks[networkId];
-      setContract(new web3.eth.Contract(VerifyCertificate.abi, networkData.address, { from: window.ethereum.selectedAddress }));
-    }
-
-    loadContract();
-  }, [])
 
   return (
     <div class="my-20 flex justify-center w-full">
