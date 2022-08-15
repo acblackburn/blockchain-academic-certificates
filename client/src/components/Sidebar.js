d552eed7b8b43cf46633a3d19844e249e5279c44
acblackburn <alexcblackburn98@gmail.com>
2022-08-15 12:51:25 +0100

    Add routing and styling to sidebar

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -2,8 +2,24 @@ import { Link } from 'react-router-dom';
 
 const Sidebar = (props) => {
   return (
-    <aside class="sticky top-0 left-0 h-screen w-32 bg-teal-500">
-      Test
+    <aside class="flex flex-col sticky top-0 left-0 h-screen w-24 overflow-hidden bg-teal-500">
+      <nav class="flex flex-col">
+        <Link to="/" class="flex flex-col items-center justify-center h-20 w-full hover:bg-teal-600 transition hover:duration-100">
+            HOME
+        </Link>
+        <Link to="/verify" class="flex flex-col items-center justify-center h-20 w-full hover:bg-teal-600 transition hover:duration-100">
+          VERIFY
+        </Link>
+        <Link to="/publish" class="flex flex-col items-center justify-center h-20 w-full hover:bg-teal-600 transition hover:duration-100">
+          PUBLISH
+        </Link>
+        <Link to="/view" class="flex flex-col items-center justify-center h-20 w-full hover:bg-teal-600 transition hover:duration-100">
+          VIEW
+        </Link>
+        <Link to="/gun-test" class="flex flex-col items-center justify-center h-20 w-full hover:bg-teal-600 transition hover:duration-100">
+          gun-test
+        </Link>
+      </nav>
     </aside>
   );
 }
