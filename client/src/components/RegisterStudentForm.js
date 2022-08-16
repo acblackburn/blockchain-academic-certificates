aa567840151062d309d667f3986312a9e99caf86
acblackburn <alexcblackburn98@gmail.com>
2022-08-16 20:03:52 +0100

    Connect the student register from with gunDB to store student accounts

--- client/src/components/RegisterStudentForm.js
+++ client/src/components/RegisterStudentForm.js
@@ -5,15 +5,27 @@ const RegisterStudentForm = (props) => {
 
   const [firstName, setFirstName] = useState("");
   const [surname, setSurname] = useState("");
+  const [existingStudentName, setExistingStudentName] = useState("");
   const firstNameRef = useRef("");
   const surnameRef = useRef("");
 
   const gun = Gun({peers: ['http://localhost:3001/gun']});
   const studentAccounts = gun.get('studentAccounts');
 
+  useEffect(() => {
+    if (props.account) {
+      studentAccounts.get(props.account).on((studentName, account) => {
+        if (studentName) {
+          setExistingStudentName(studentName);
+        }
+      });
+    }
+  })
+
   const handleRegisterAccount = (e) => {
     e.preventDefault();
     console.log(firstName + " " + surname);
+    studentAccounts.get(props.account).put(firstName + " " + surname);
     setFirstName("");
     setSurname("");
     firstNameRef.current.value = "";
@@ -22,9 +34,7 @@ const RegisterStudentForm = (props) => {
 
   return (
     <form onSubmit={handleRegisterAccount} class="flex flex-col p-10 justify-center bg-white drop-shadow">
-      <div class="block tracking-wide text-gray-700 text-s mx-2 mb-2">
-        Adding student details for blockchain account: <strong>{props.account.substring(0,5)}...{props.account.substring(38.42)}</strong>
-      </div>
+      <FormInfoText account={props.account} existingStudentName={existingStudentName} />
       <div class="flex flex-row">
         <div class="flex flex-col mx-2">
           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
@@ -48,6 +58,30 @@ const RegisterStudentForm = (props) => {
       </div>
     </form>
   );
+}
+
+const FormInfoText = (props) => {
+  if (props.account) {
+    return (
+      <div>
+        <div class="block tracking-wide text-gray-700 text-s mx-2 mb-4">
+          Adding student details for blockchain account: <strong>{props.account.substring(0,5)}...{props.account.substring(38.42)}</strong>
+        </div>
+        {props.existingStudentName !== "" &&
+          <div class="block tracking-wide text-red-600 text-s mx-2 mb-6">
+            This blockchain account is already registered for student name: <strong>{props.existingStudentName}</strong><br/>
+            If you would like to update this name, please <strong>submit a new one.</strong>
+          </div>
+        }
+      </div>
+    );
+  } else {
+    return (
+      <div class="block tracking-wide text-red-600 text-s mx-2 mb-6">
+        No blockchain account detected. Please make sure you have MetaMask set up<br/> and then click the <strong>Connect Wallet</strong> button.
+      </div>
+    );
+  }
 }
 
 export default RegisterStudentForm;
